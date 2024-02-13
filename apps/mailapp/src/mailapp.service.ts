8d3e8c53cdba12f01c8e9a4216e77794143cb4e6
claudiomelendres <claudiomelendres@gmail.com>
2024-02-13 16:21:43 -0400

    added config

--- apps/mailapp/src/mailapp.service.ts
+++ apps/mailapp/src/mailapp.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MailappService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! This is the mail app!';
   }
 }
--- apps/mailapp/src/main.ts
+++ apps/mailapp/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { MailappModule } from './mailapp.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MailappModule);
-  await app.listen(3001);
-  console.log('mail app is running on port 3001');
+  // const app = await NestFactory.create(MailappModule);
+  // await app.listen(3001);
+  // console.log('mail app is running on port 3001');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MailappModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+  await app.listen();
 }
 bootstrap();
