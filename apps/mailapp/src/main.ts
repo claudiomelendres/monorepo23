8d54fca56217a5ae01c45c92b6927933793ac859
claudiomelendres <claudiomelendres@gmail.com>
2024-02-14 16:42:24 -0400

    added rabbitMQ config

--- apps/mailapp/src/main.ts
+++ apps/mailapp/src/main.ts
@@ -9,10 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     MailappModule,
     {
-      transport: Transport.REDIS,
+      transport: Transport.RMQ,
       options: {
-        host: 'localhost',
-        port: 6379,
+        urls: ['amqp://localhost:5672'],
+        queue: 'mails_queue',
+        queueOptions: {
+          durable: false
+        },
       },
     },
   );
--- apps/main-nest/src/app.module.ts
+++ apps/main-nest/src/app.module.ts
@@ -8,11 +8,14 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
     ClientsModule.register([
       {
         name: 'MAIL_SERVICE',
-        transport: Transport.REDIS,
+        transport: Transport.RMQ,
         options: {
-          host: 'localhost',
-          port: 6379,
-        }
+          urls: ['amqp://localhost:5672'],
+          queue: 'mails_queue',
+          queueOptions: {
+            durable: false
+          },
+        },
       },
     ]),
   ],
