ee38308b72c56a7e203936fe76680a5592ea1a7c
claudiomelendres <claudiomelendres@gmail.com>
2024-02-14 12:28:44 -0400

    added eventPattern and redis compose

--- apps/mailapp/src/mailapp.controller.ts
+++ apps/mailapp/src/mailapp.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { MailappService } from './mailapp.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class MailappController {
-  constructor(private readonly mailappService: MailappService) {}
+  constructor(private readonly mailappService: MailappService) { }
 
-  @Get()
-  getHello(): string {
-    return this.mailappService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.mailappService.getHello();
+  // }
+
+
+  @EventPattern('new_email')
+  async handleUserCreated(data: any) {
+    console.log('this is the incoming event', data);
   }
 }
--- apps/main-nest/src/app.service.ts
+++ apps/main-nest/src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+
+  constructor(@Inject('MAIL_SERVICE') private clientMail: ClientProxy) { }
+
   getHello(): string {
     return 'Hello World! main-nest app!';
+  }
+
+  newUser(user: any): string {
+
+    this.clientMail.emit('new_email', user);
+    return 'send_queue';
+
   }
 }
